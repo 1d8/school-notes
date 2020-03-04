c1275b7e932da60ec323b21618e494448e89e4ae
1d8 <56985822+1d8@users.noreply.github.com>
2020-03-04 13:30:37 -0800

    Update variance.py

--- variance.py
+++ variance.py
@@ -1,6 +1,7 @@
 mean = 6.2
 nums = [9,7,4,2,8,7,6,7,8,3,4,7,8,7,5,6,7,3,7,9]
-
+#this calculates the deviation, not the variance
+#from here, you add up all the deviations & divide by (N-1) (where N = total number of  scores) to find the variance.
 for i in nums:
     i = int(i)
     deviation = mean - i
