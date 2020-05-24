648995fa66b3333acd4aa6fafa0c1954068dea22
1d8 <56985822+1d8@users.noreply.github.com>
2020-05-24 15:07:21 -0700

    Update covariance.py

--- covariance.py
+++ covariance.py
@@ -1,47 +1,35 @@
 # for vars x & y, fill these w/your data
 
-x = []
-y = []
+x = [20, 22, 4.4]
+y = [78, 73, 71]
 x_deviation = []
 y_deviation = []
-x_mean = 12.56
-y_mean = 2.04
+x_mean = 15.5
+y_mean = 74
 
-## Variance Section
-
-# finding variance for x
+#IF YOU"RE FINDING VARIANCE, UNCOMMENT THE DEVIATION=DEVIATION**2 LINES
+# finding deviations for x
 #If you're attempting to find covariance, comment out the deviation = deviation ** 2 part on both the x loop & y loop
 for i in x:
 	deviation = i - x_mean
-	deviation = deviation ** 2
+	#deviation = deviation ** 2
 	x_deviation.append(deviation)
-
-#prints the sum for your x variance, then you divide by N-1
-z = sum(x_deviation)
-print(z)
-
-# finding variance for y
+print("x deviations:", x_deviation)
+# finding deviations for y
 for i in y:
 	deviation = i - y_mean
-	deviation = deviation ** 2
+	#deviation = deviation ** 2
 	y_deviation.append(deviation)
-
-#prints the sum for your y variance, then you divide by N-1
-z = sum(y_deviation)
-print(z)
-
-
-## Covariance
-multiplied_num = []
-#Make sure to comment out the deviation**2 parts of the code above if you're attempting to find the covariance
+print("y deviations", y_deviation)
+#covariation
 # Change the range int according to the length of the X & Y values, this will iterate through the multiplication part 
 # of the equation 25 times in order to multiply every number, then it adds them to the Z variable
-#for i in range(25):
-#	number = x_deviation[i] * y_deviation[i]
-#	multiplied_num.append(number)
-
-# prints sum for your x & y covariance, then you divide by N-1
-#z = sum(multiplied_num)
-#print(z)
-
-### by the population size (N) - 1
+multiplied_num = []
+for i in range(3):
+	number = x_deviation[i] * y_deviation[i]
+	multiplied_num.append(number)
+z = sum(multiplied_num)
+print("adding up all the values after multiplying them gets:", z)
+n = len(x) - 1
+covariance = z/n
+print("Your covariance is:", covariance)
